d746921c5b23c25f599f86dc00db0b921a251f23
superkush06 <superkush06@gmail.com>
2026-06-16 08:54:53 -0400

    feat: flat public API at the bandit package root

--- bandit/__init__.py
+++ bandit/__init__.py
@@ -1,3 +1,23 @@
 """gauss-bandit: multi-armed bandits library."""
 
+from .algos import (
+    EXP3,
+    UCB1,
+    EpsilonGreedy,
+    ThompsonBernoulli,
+    ThompsonGaussian,
+    annealed,
+)
+from .envs import BanditEnv, BernoulliBandit, GaussianBandit
+from .metrics import cumulative_pseudo_regret, cumulative_regret, lai_robbins_lower_bound
+from .runner import ExperimentResult, RunResult, run_experiment, run_one
+
 __version__ = "0.1.0"
+__all__ = [
+    "BanditEnv", "BernoulliBandit", "GaussianBandit",
+    "EpsilonGreedy", "UCB1", "ThompsonBernoulli", "ThompsonGaussian", "EXP3",
+    "annealed",
+    "cumulative_regret", "cumulative_pseudo_regret", "lai_robbins_lower_bound",
+    "run_one", "run_experiment", "RunResult", "ExperimentResult",
+    "__version__",
+]
